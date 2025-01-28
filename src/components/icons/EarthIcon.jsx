9598f7591107590d5e36141c520b1a254dbb20a7
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-01-28 15:08:11 +0100

    fix image rendering

--- src/components/icons/EarthIcon.jsx
+++ src/components/icons/EarthIcon.jsx
@@ -1,5 +1,3 @@
 export default function EarthIcon() {
-  return (
-    <img src='public/earth.svg' alt='Earth Icon' className='h-10 sm:h-14' />
-  );
+  return <img src="/earth.svg" alt="Earth Icon" className="h-10 sm:h-14" />;
 }
